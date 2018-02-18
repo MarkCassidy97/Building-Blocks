9b79a4b10fd5a3ef0f3c0797eba6656791bd109c
Mark <m.cassidy@amaze.com>
2018-02-18 16:40:44 +0000

    updated

--- scripts/g-map.js
+++ scripts/g-map.js
@@ -1,22 +1,56 @@
-// var localSearch = new GlocalSearch();
+var map;
+function initMap() {
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 9,
+    center: {
+      lat: 53.47282,
+      lng: -2.24651
+    }
+  });
+  var geocoder = new google.maps.Geocoder();
 
-// $('script').load(function () {alert() }); 
+  document.getElementById('search').addEventListener('click', function () {
+    geocodeAddress(geocoder, map);
+  });
+  document.getElementById('use-location').addEventListener('click', function () {
+    getLocation();
+  });
+  function getLocation() {
+    if (navigator.geolocation) {
+      console.log("hola");
+      navigator.geolocation.getCurrentPosition(showPosition);
 
+    } else {
+      getUserLocation.innerHTML = "Geolocation is not supported by this browser.";
+    }
+  }
 
-// function usePointFromPostcode(postcode, callbackFunction) {
-    
-//     localSearch.setSearchCompleteCallback(null, 
-//       function() {
-        
-//         if (localSearch.results[0]) {    
-//           var resultLat = localSearch.results[0].lat;
-//           var resultLng = localSearch.results[0].lng;
-//           var point = new GLatLng(resultLat,resultLng);
-//           callbackFunction(point);
-//         }else{
-//           alert("Postcode not found!");
-//         }
-//       });  
-      
-//     localSearch.execute(postcode + ", UK");
-//   }
+  function showPosition(position) {
+    console.log("hello");
+    var lat = position.coords.latitude;
+    var lng = position.coords.longitude;
+    map.setCenter(new google.maps.LatLng(lat, lng));
+    var marker = new google.maps.Marker({
+      position: new google.maps.LatLng(lat, lng),
+      map: map,
+    });
+  }
+}
+
+function geocodeAddress(geocoder, resultsMap) {
+  var address = document.getElementById('postcode-box').value;
+  geocoder.geocode({
+    'address': address
+  }, function (results, status) {
+    if (status === 'OK') {
+      resultsMap.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+        map: resultsMap,
+        position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+
+}
--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,72 +1,41 @@
-function initMap() {
+$(document).ready(function () {
   
-      var geocoder = new google.maps.Geocoder();
-  
-      var locations = [
-          ['Chester', 53.183497, -2.890605, 3, 'https://www.google.co.uk'],
-          ['Manchester', 53.474103, -2.243593, 2, 'https://www.google.co.uk'],
-          ['Liverpool', 53.421206, -2.945146, 1, 'https://www.google.co.uk/']
-      ];
-
-      var dealerships = {
-        title: "METROPOLIS MOTORCYCLES",
-        title: "CARL ROSNER MOTORCYCLES",
-        city: "Manchester",
-        region: "North west"
-      }
-      
-      
-  
-      var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 9,
-          center: new google.maps.LatLng(53.381021, -2.608138),
-          mapTypeId: google.maps.MapTypeId.TERRAIN,
-  
-      });
-  
-      var infowindow = new google.maps.InfoWindow();
-  
-  
-      for (var i = 0; i < locations.length; i++) {
-          var latLng = { lat: locations[i][1], lng: locations[i][2] };
-          resolveAddressByLatLng(geocoder, latLng, (function(loc) {
-  
-                  return function(result) {
-                      //console.log(location);
-  
-                      var marker = new google.maps.Marker({
-                          position: new google.maps.LatLng(loc[1], loc[2]),
-                          url: loc[4],
-                          map: map
-                      });
-  
-  
-                      google.maps.event.addListener(marker, 'click', function() {
-                          infowindow.setContent(result[0].formatted_address);
-                          infowindow.open(map, marker);
-                      });
-  
-                  }
-  
-              })(locations[i]),
-              function(status) {
-                  console.log("Geocode was not successful for the following reason: " + status);
-              });
-  
-  
-      }
-  }
-  
-  
-  function resolveAddressByLatLng(geocoder, latLng,success,error) {
-      geocoder.geocode({ 'location': latLng }, function (results, status) {
-          if (status == google.maps.GeocoderStatus.OK) {
-              success(results);
-          } else {
-              error(status);
-          }
-      });
-  }
 
 
-  
+  var data = [];
+  //Currently gets the box ID
+  $(".address-box").click(function () {
+
+    var box = {};
+
+    // get all the nested children
+    var children = $(this).children('.address-data').children()
+
+    // iterate through each and make an associative array with the text
+    $.each(children, function (value) {
+      var className = $(this).attr('class');
+      box[className] = $(this).text();
+    });
+    console.log(box);
+  });
+
+  $("#next-step-button").on('click', function(){
+    
+      window.location='index2.html';
+    });
+
+  });
+
+
+
+function validateForm() {
+  var isValid = true;
+  $('#form input').each(function () {
+    if ($(this).val() === '')
+      isValid = false;
+  });
+
+  return isValid;
+
+}
+
